b2d0d74029bf4ee10b1288fd1cbf2b07be5cc339
C.S.Dhananjay <161456374+dhananjay97mki@users.noreply.github.com>
2025-01-11 21:52:15 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,2 @@
+import ui from './module2.mjs'; // Correct the typo in the import statement
+console.log(ui);
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+const a = "Yuri"; 
+const b = "Optimus prime";
+const c = "Wolverine";
+const d = "Batman"; 
+
+export default a;
